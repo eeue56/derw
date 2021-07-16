0907111fa0b822d164b9758896058648483a41a6
eeue56 <enalicho@gmail.com>
2021-07-16 17:03:02 +0200

    fix block splitting for functions

--- src/blocks.ts
+++ src/blocks.ts
@@ -1,15 +1,33 @@
+function isFunctionOrConst(line: string): boolean {
+    return !line.startsWith("type ") && !line.startsWith(" ");
+}
+
 export function intoBlocks(body: string): string[] {
     const blocks = [ ];
 
     let currentBlock = [ ];
+    let isInBlock = false;
     const lines = body.split("\n");
     for (var i = 0; i < lines.length; i++) {
         const line = lines[i];
-        if (currentBlock.length > 0 && !line.startsWith(" ")) {
-            blocks.push(currentBlock.join("\n").trim());
-            currentBlock = [ ];
+        const isPreviousFunctionDef =
+            i === 0 ? false : isFunctionOrConst(lines[i - 1]);
+        const inFunctionDef = isFunctionOrConst(line);
+        const isIndentedLine = line.startsWith(" ");
+
+        if (isInBlock) {
+            if (isIndentedLine || (inFunctionDef && isPreviousFunctionDef)) {
+                currentBlock.push(line);
+            } else {
+                blocks.push(currentBlock.join("\n").trim());
+                currentBlock = [ ];
+                isInBlock = false;
+            }
         } else {
-            currentBlock.push(line);
+            if (!isIndentedLine) {
+                currentBlock.push(line);
+                isInBlock = true;
+            }
         }
     }
 
